2b83fe9f7a50210d03308cbbfddb02fd4eba61ea
meng <763577618@qq.com>
2019-07-15 17:25:03 +0800

    功能完成

--- app/src/main/java/com/chinastis/numberselector/MainActivity.java
+++ app/src/main/java/com/chinastis/numberselector/MainActivity.java
@@ -1,7 +1,10 @@
 package com.chinastis.numberselector;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.horizontalselectedviewlibrary.HorizontalselectedView;
 
@@ -10,10 +13,22 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.text_main);
+        NumberSelector numberSelector = (NumberSelector) findViewById(R.id.hd_main);
+
+        numberSelector.setOnNumberChangeListener(new NumberSelector.OnNumberChangeListener() {
+            @Override
+            public void onNumberChanged(float number) {
+
+                textView.setText(number+" 公斤");
+            }
+        });
 
 
     }
--- app/src/main/java/com/chinastis/numberselector/NumberSelector.java
+++ app/src/main/java/com/chinastis/numberselector/NumberSelector.java
@@ -3,23 +3,31 @@ package com.chinastis.numberselector;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.NumberPicker;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by xianglong on 2019/7/13.
  */
+
 
 public class NumberSelector extends LinearLayout {
 
@@ -28,19 +36,44 @@ public class NumberSelector extends LinearLayout {
 
     private float width;
     private float height;
+    private int strokeWidth;
 
     private int textSize;
     private int color;
 
-    private float maxWeight;
-    private float minWeight;
+    private int maxWeight;
+    private int minWeight;
     private float range;
 
     private RecyclerView degreeRV;
     private RecyclerView numberRV;
 
-    private List<Float> numberList;
-    private List<Float> degreeList;
+    private List<Integer> numberList = new ArrayList<>();
+    private List<Integer> degreeList = new ArrayList<>();
+
+    private final int NUMBER_TYPE = 0;
+    private final int DEGREE_TYPE = 1;
+
+    private LinearLayoutManager layoutManagerDegree;
+    private LinearLayoutManager layoutManagerNumber;
+    private int selectedPosition = -1;
+
+    private int selectedPositionNum = -1;
+
+    private SparseArray<ImageView> imageViews = new SparseArray<>();
+    private SparseArray<TextView> numberTexts = new SparseArray<>();
+
+    private Drawable degree;
+    private Drawable degree1;
+    private Drawable degree2;
+    private Drawable degree3;
+
+
+    private OnNumberChangeListener onNumberChangeListener;
+
+    public void setOnNumberChangeListener(OnNumberChangeListener listener) {
+        this.onNumberChangeListener = listener;
+    }
 
     public NumberSelector(Context context) {
         super(context);
@@ -51,36 +84,173 @@ public class NumberSelector extends LinearLayout {
         this.context = context;
         LayoutInflater.from(context).inflate(R.layout.number_selector_layout,this);
 
-
         TypedArray ta = context.obtainStyledAttributes(attrs,R.styleable.NumberSelector);
-        maxWeight = ta.getFloat(R.styleable.NumberSelector_max_number,10);
-        minWeight = ta.getFloat(R.styleable.NumberSelector_min_number,0);
+        maxWeight = ta.getInteger(R.styleable.NumberSelector_max_number,100);
+        minWeight = ta.getInteger(R.styleable.NumberSelector_min_number,0);
+        strokeWidth = ta.getInteger(R.styleable.NumberSelector_stroke_width,4);
         range = ta.getFloat(R.styleable.NumberSelector_range,0.1f);
         ta.recycle();
         initData();
         intView();
         intiPaint();
+        setWillNotDraw(false);
+        Log.e("MENG","init ");
 
     }
 
     private void initData() {
-        for (float f = minWeight; f <= maxWeight; f += range) {
-            degreeList.add(f);
 
-            Log.e("MENG","度数："+f);
+        for (int i = 0 ;i<maxWeight*10;i++) {
+            degreeList.add(i);
 
-            if ((f-minWeight)%range==0) {
-                numberList.add(f);
-                Log.e("MENG","数字："+f);
+            if (i%10 == 0) {
+                numberList.add(i/10);
             }
         }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+//        setMeasuredDimension((int)width,(int)height);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        if (widthMode == MeasureSpec.EXACTLY ) {
+            width = widthSize;
+        } else {
+            width = getMeasuredWidth();
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY ) {
+            height = heightSize;
+        } else {
+            height = getMeasuredHeight();
+        }
+
 
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onDraw(Canvas canvas) {
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(strokeWidth/2,strokeWidth/2,height-strokeWidth/2,height-strokeWidth/2,
+                90,180,false,paint);
+        canvas.drawArc(width-height-strokeWidth,strokeWidth/2,width-strokeWidth/2,height-strokeWidth/2,
+                -90,180,false,paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(height/2,0,width-height/2,strokeWidth,paint);
+        canvas.drawRect(height/2,height-strokeWidth,width-height/2,height,paint);
+
+
+
+        Log.e("MENG","on draw");
+        super.onDraw(canvas);
+    }
+
 
     private void intView() {
         degreeRV = (RecyclerView) this.findViewById(R.id.degree_rv);
         numberRV = (RecyclerView) this.findViewById(R.id.number_rv);
 
+        degree = context.getResources().getDrawable(R.drawable.degree);
+        degree1 = context.getResources().getDrawable(R.drawable.degree1);
+        degree2 = context.getResources().getDrawable(R.drawable.degree2);
+        degree3 = context.getResources().getDrawable(R.drawable.degree3);
+
+        layoutManagerDegree = new LinearLayoutManager(context);
+        layoutManagerDegree.setOrientation(LinearLayoutManager.HORIZONTAL);
+        degreeRV.setLayoutManager(layoutManagerDegree);
+
+        layoutManagerNumber = new LinearLayoutManager(context);
+        layoutManagerNumber.setOrientation(LinearLayoutManager.HORIZONTAL);
+        numberRV.setLayoutManager(layoutManagerNumber);
+
+        degreeRV.setAdapter(new NumberSelectorAdapter(degreeList,context,DEGREE_TYPE));
+        numberRV.setAdapter(new NumberSelectorAdapter(numberList,context,NUMBER_TYPE));
+
+        degreeRV.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (recyclerView.getScrollState() != RecyclerView.SCROLL_STATE_IDLE) {
+                    numberRV.scrollBy(dx, dy);
+                    refreshSelectNumber();
+                }
+            }
+        });
+
+        numberRV.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (recyclerView.getScrollState() != RecyclerView.SCROLL_STATE_IDLE) {
+                    degreeRV.scrollBy(dx, dy);
+                    refreshSelectNumber();
+
+                }
+            }
+        });
+
+        numberRV.scrollToPosition(numberList.size()/2);
+        degreeRV.scrollToPosition(degreeList.size()/2);
+
+    }
+
+    private void refreshSelectNumber() {
+        int first = layoutManagerDegree.findFirstVisibleItemPosition();
+        int last = layoutManagerDegree.findLastVisibleItemPosition();
+        int oldSelectedPosition = selectedPosition;
+        selectedPosition = (last-first)/2+first;
+
+        if (onNumberChangeListener!=null) {
+            float ori = degreeList.get(selectedPosition);
+            float zhengshu = ori/10;
+
+            onNumberChangeListener.onNumberChanged(zhengshu);
+        }
+
+        if (selectedPosition!= oldSelectedPosition) {
+            try {
+                imageViews.get(selectedPosition).setImageDrawable(degree1);
+                imageViews.get(selectedPosition-1).setImageDrawable(degree2);
+                imageViews.get(selectedPosition+1).setImageDrawable(degree2);
+                imageViews.get(selectedPosition-2).setImageDrawable(degree3);
+                imageViews.get(selectedPosition+2).setImageDrawable(degree3);
+            } catch (Exception e) {
+                Log.e("MENG","null:"+e);
+            }
+
+            for (int i = 0; i<imageViews.size(); i++) {
+                if (selectedPosition-imageViews.keyAt(i)>2 || selectedPosition-imageViews.keyAt(i)<-2) {
+                    imageViews.valueAt(i).setImageDrawable(degree);
+                }
+            }
+
+            if (selectedPosition % 10 == 0) {
+                selectedPositionNum = selectedPosition/10;
+
+                if (numberTexts.get(selectedPositionNum)!=null) {
+                    numberTexts.get(selectedPositionNum).setTextColor(Color.rgb(242, 131, 76));
+                }
+
+
+            } else {
+                if (numberTexts.get(selectedPositionNum)!=null) {
+                    numberTexts.get(selectedPositionNum).setTextColor(Color.LTGRAY);
+                }
+            }
+
+        }
 
     }
 
@@ -93,66 +263,97 @@ public class NumberSelector extends LinearLayout {
     private void intiPaint() {
         this.paint = new Paint();
         paint.setAntiAlias(true);
-    }
-
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-
-        if (widthMode == MeasureSpec.EXACTLY) {
-            width = MeasureSpec.getSize(widthMeasureSpec);
-        } else {
-            width = 400;
-        }
-
-        if (heightMode == MeasureSpec.EXACTLY) {
-            height = MeasureSpec.getSize(heightMeasureSpec);
-        } else {
-            height = 80;
-        }
-
-        setMeasuredDimension((int)width,(int)height);
+        paint.setColor(Color.LTGRAY);
+        paint.setStrokeWidth(strokeWidth);
 
     }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-    }
-
-
-
 
     class NumberSelectorAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-        private List<Float> data;
+        private int totalHolderCount = 0;
+        private int totalTextCount = 0;
+        private List<Integer> data;
         private Context mContext;
+        private LayoutInflater inflater;
 
+        private int type;
+
+        public NumberSelectorAdapter(List<Integer> data, Context mContext, int type) {
+            this.data = data;
+            this.mContext = mContext;
+            this.type = type;
+            this.inflater = LayoutInflater.from(this.mContext);
+        }
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            View itemView;
+
+            if (type == DEGREE_TYPE) {
+                totalHolderCount++;
+                itemView = inflater.inflate(R.layout.item_degree,parent,false);
+            } else {
+                totalTextCount++;
+                itemView = inflater.inflate(R.layout.item_number,parent,false);
+            }
+
+            RecyclerView.ViewHolder holder = new NumberSelectorViewHolder(itemView,type);
+
+            return holder;
         }
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+            NumberSelectorViewHolder viewHolder = (NumberSelectorViewHolder) holder;
+            if (type == DEGREE_TYPE) {
+                imageViews.put(position,viewHolder.image);
+                if (imageViews.size()>totalHolderCount) {
+                    imageViews.remove(position-totalHolderCount);
+                    imageViews.remove(position+totalHolderCount);
+                }
+
+
+            }  else {
+                numberTexts.put(position,viewHolder.text);
+                if (numberTexts.size()>totalTextCount) {
+                    numberTexts.remove(position-totalTextCount);
+                    numberTexts.remove(position+totalTextCount);
+                }
+
+                if (data.get(position) < 10)
+                    viewHolder.text.setText("" + data.get(position));
+                else
+                    viewHolder.text.setText(data.get(position) + "");
+
+            }
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return data == null ? 0 : data.size();
         }
 
 
         class NumberSelectorViewHolder extends RecyclerView.ViewHolder {
 
-            public NumberSelectorViewHolder(View itemView) {
+            TextView text;
+            ImageView image;
+
+            public NumberSelectorViewHolder(View itemView,int type) {
                 super(itemView);
+                if (type == DEGREE_TYPE) {
+                    image = (ImageView) itemView.findViewById(R.id.item_degree_text);
+                } else {
+                    text = (TextView) itemView.findViewById(R.id.item_number_text);
+
+                }
             }
         }
+    }
+
+
+    interface OnNumberChangeListener {
+        void onNumberChanged(float number);
     }
 }
